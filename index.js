09573b5404a10e631a0faf88508df90f90781a1e
lestersphinx76 <167202189+lestersphinx76@users.noreply.github.com>
2024-04-16 21:25:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -74,14 +74,14 @@ application.get('/fetch', async (
             const information = await NobloxJS.getPlayerInfo(Number(profileIdentifier));
 
             return {
-                accountAge: information.age ?? 'Account age not specified',
+                accountAge: formatNumber(information.age) ?? 'Account age not specified',
                 description: information.blurb ?? 'No description added',
                 displayName: information.displayName ?? information.username,
-                followerCount: information.followerCount ?? 'No followers',
-                followingCount: information.followingCount ?? 'User not following anyone',
-                friendCount: information.friendCount ?? 'No friends yet',
+                followerCount: formatNumber(information.followerCount) ?? 'No followers',
+                followingCount: formatNumber(information.followingCount) ?? 'User not following anyone',
+                friendCount: formatNumber(information.friendCount) ?? 'No friends yet',
                 accountBanned: information.isBanned ? 'Account is banned!' : 'Account is not banned.',
-                joinDate: information.joinDate ?? 'Join date not specified',
+                joinDate: formatDate(information.joinDate) ?? 'Join date not specified',
                 previousNames: information.oldNames.join(', ') ?? 'No previous names',
                 username: information.username
             };
@@ -110,14 +110,14 @@ application.get('/fetch', async (
         'fetch',
 
         {
-            accountAge: formatNumber(Number(information.accountAge)),
+            accountAge: information.accountAge,
             description: information.description,
             displayName: information.displayName,
-            followerCount: formatNumber(Number(information.followerCount)),
-            followingCount: formatNumber(Number(information.followingCount)),
-            friendCount: formatNumber(Number(information.friendCount)),
+            followerCount: information.followerCount,
+            followingCount: information.followingCount,
+            friendCount: information.friendCount,
             accountBanned: information.accountBanned,
-            joinDate: formatDate(new Date(information.joinDate)),
+            joinDate: information.joinDate,
             previousNames: information.previousNames,
             username: information.username
         }
