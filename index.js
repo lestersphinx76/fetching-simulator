2987c0d64190c052483f98c34ea993aea040bc86
lestersphinx76 <167202189+lestersphinx76@users.noreply.github.com>
2024-04-16 21:09:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,20 +33,71 @@ application.get('/fetch', async (
 ) => {
     const { identifier } = request.query;
 
+    function formatNumber (number) {
+        return number.toString().replace(
+            /\B(?=(\d{3})+(?!\d))/g,
+            ','
+        );
+    };
+
+    function formatDate (date) {
+        const monthNames = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+
+        const month = monthNames[date.getMonth()];
+        const day = date.getDate();
+        const year = date.getFullYear();
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+
+        const period = (hour < 12) ? 'AM' : 'PM';
+        const formattedHour = (hour % 12) ? 12 : hour % 12;
+        const formattedMinute = (minute < 10) ? `0${minute}` : minute;
+
+        return `${month} ${day}, ${year} at ${formattedHour}:${formattedMinute} ${period}`;
+    };
+
     async function getFetchedProfileInformation (profileIdentifier) {
         const information = await NobloxJS.getPlayerInfo(Number(profileIdentifier));
 
-        return {
-            accountAge: information.age ?? 'Account age not specified',
-            description: information.blurb ?? 'No description added',
-            displayName: information.displayName ?? information.username,
-            followerCount: information.followerCount ?? 'No followers',
-            followingCount: information.followingCount ?? 'User not following anyone',
-            friendCount: information.friendCount ?? 'No friends yet',
-            accountBanned: information.isBanned ? 'Account is banned!' : 'Account is not banned.',
-            joinDate: information.joinDate ?? 'Join date not specified',
-            previousNames: information.oldNames.join(', ') ?? 'No previous names',
-            username: information.username
+        if (information) {
+            return {
+                accountAge: information.age ?? 'Account age not specified',
+                description: information.blurb ?? 'No description added',
+                displayName: information.displayName ?? information.username,
+                followerCount: information.followerCount ?? 'No followers',
+                followingCount: information.followingCount ?? 'User not following anyone',
+                friendCount: information.friendCount ?? 'No friends yet',
+                accountBanned: information.isBanned ? 'Account is banned!' : 'Account is not banned.',
+                joinDate: information.joinDate ?? 'Join date not specified',
+                previousNames: information.oldNames.join(', ') ?? 'No previous names',
+                username: information.username
+            };
+        } else {
+            return {
+                accountAge: 'Account age not specified',
+                description: 'No description added',
+                displayName: 'Unknown',
+                followerCount: 'No followers',
+                followingCount: 'User not following anyone',
+                friendCount: 'No friends yet',
+                accountBanned: 'Not specified if banned or not',
+                joinDate: 'Join date not specified',
+                previousNames: 'No previous names',
+                username: 'Unknown'
+            };
         };
     };
 
@@ -56,14 +107,14 @@ application.get('/fetch', async (
         'fetch',
 
         {
-            accountAge: information.accountAge,
+            accountAge: formatNumber(Number(information.accountAge)),
             description: information.description,
             displayName: information.displayName,
-            followerCount: information.followerCount,
-            followingCount: information.followingCount,
-            friendCount: information.friendCount,
+            followerCount: formatNumber(Number(information.followerCount)),
+            followingCount: formatNumber(Number(information.followingCount)),
+            friendCount: formatNumber(Number(information.friendCount)),
             accountBanned: information.accountBanned,
-            joinDate: information.joinDate,
+            joinDate: formatDate(new Date(information.joinDate)),
             previousNames: information.previousNames,
             username: information.username
         }
